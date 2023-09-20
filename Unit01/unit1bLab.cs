0840c926e7d4018976e0d836b67594788349878c
Calvin <calvinturville@gmail.com>
2023-09-20 16:50:16 -0600

    saving again

--- Unit01/unit1bLab.cs
+++ Unit01/unit1bLab.cs
@@ -4,17 +4,17 @@ public class Program
 {
 	public void Main()
 	{
-		Console.WriteLine("What's the weather in celcius today? ");
-		var number = Console.ReadLine();
-		int value = Convert.ToInt32(number);
-		CheckWeather(value);
+		Console.WriteLine("What's the weather in celcius today? "); // Asking question in command line
+		var number = Console.ReadLine(); // Reading user input
+		int value = Convert.ToInt32(number); // Changing string into int
+		CheckWeather(value); // running the void function
 	}
 	
-	public void CheckWeather (int degree) {
-		if (degree >= 30){
+	public void CheckWeather (int degree) { // Whenever CheckWeather is used it runs this function
+		if (degree >= 30) { // Asking is user answer is greater than or equal to 30
 			Console.WriteLine("Wear something light. ");
 		}
-		else {
+		else { // if not greater than or equal to 30 then writing this string
 			Console.WriteLine("Wear something heavy. ");
 		}
 	}
